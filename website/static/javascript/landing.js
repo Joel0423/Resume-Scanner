04cdebfbf052f0f138004b8f84a01e759061137c
Mahalakshmi0908 <mahalakshmi.c@mca.christuniversity.in>
2025-02-11 13:35:39 +0530

    You can select the text and click on buttons now

--- website/static/javascript/landing.js
+++ website/static/javascript/landing.js
@@ -3,3 +3,9 @@
     console.log("DOM fully loaded and parsed");
   });
  */
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        document.body.classList.add("css-loaded")
+    });
+</script>
